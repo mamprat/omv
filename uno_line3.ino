7b3651457908d0efaafaf513062e5fc2b792c5cc
Imam Pratama Setiady <88639642+mamprat@users.noreply.github.com>
2024-08-12 10:45:10 +0700

    update batas kirim

--- uno_line3.ino
+++ uno_line3.ino
@@ -1,4 +1,69 @@
 #include "EmonLib.h"
+
+const int sensor = 1;  //A1
+const int pinReset = 8;
+
+EnergyMonitor emon1;
+const int threshold = 100;
+
+static unsigned long OnDelay;
+unsigned long TimeOn = 20000;
+
+void (*reset)(void) = 0;
+int printCounter = 0;
+int dataPrint = 10;
+
+int lastState = -1;  //Menyimpan status terakhir, diinisialisasi dengan nilai yang tidak valid
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    digitalWrite(pinReset, HIGH);
+    delay(100);
+    pinMode(pinReset, OUTPUT);
+  }
+  Serial.println("reset successful");
+  delay(100);
+  emon1.current(sensor, 600.600);
+  OnDelay = 0 - TimeOn;
+}
+
+void loop() {
+  int Irms = emon1.calcIrms(1480);
+  int currentState;
+
+//  if (Irms > threshold) {
+//    OnDelay = millis();
+//  }
+
+  if (millis() - OnDelay >= (TimeOn)) {
+    currentState = 0;
+  } else {
+    currentState = 1;
+  }
+
+  if (currentState != lastState) {
+    Serial.println(currentState);
+    lastState = currentState;
+    printCounter++;
+  }
+
+    if (Irms > threshold) {
+      OnDelay = millis();
+    }
+
+  if (printCounter >= dataPrint) {
+    delay(100);
+    digitalWrite(pinReset, LOW);
+    delay(100);
+    reset();
+  }
+
+  delay(500);
+}
+
+
+#include "EmonLib.h"
 #include <avr/wdt.h>
 
 const int sensor = 1;  //A1
